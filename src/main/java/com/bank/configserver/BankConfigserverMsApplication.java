21071fbd95cd81e6efe747bd92d730dac36ed317
tsanchee <tony.a.sanchez@hotmail.com>
2024-07-15 22:01:47 -0500

    first commit

---
+++ src/main/java/com/bank/configserver/BankConfigserverMsApplication.java
@@ -0,0 +1,15 @@
+package com.bank.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class BankConfigserverMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankConfigserverMsApplication.class, args);
+	}
+
+}
